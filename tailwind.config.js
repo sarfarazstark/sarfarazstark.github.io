b7091c2a65f87f042ce9461fb466ecf96d5c34c3
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2024-08-15 23:33:51 +0530

    Portfolio skill and statistics section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
 				'border-to': '#EE2A7B',
 				'border-from': '#6228D7',
 				yellow: '#F9CE34',
+				'figma-1': '#F24E1E',
+				'figma-2': '#A259FF',
+				'figma-3': '#0ACF83',
+			},
+			fontFamily: {
+				title: ['Khand', 'sans-serif'],
+				paragraph: ['Open Sans', 'sans-serif'],
 			},
 		},
 	},
