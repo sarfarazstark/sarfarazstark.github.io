38df48b4f89a684eb6ed01a48812db9dd586cec2
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2024-08-15 21:51:27 +0530

    Portfolio header completed

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const shapes = document.querySelectorAll('.shape');
-
-shapes.forEach((shape) => {
-	const size = Math.random() * 50 + 20;
-	shape.style.width = `${size}px`;
-	shape.style.height = `${size}px`;
-	shape.style.top = `${Math.random() * 100}vh`;
-	shape.style.left = `${Math.random() * 100}vw`;
-
-	setInterval(() => {
-		const x = Math.random() * 100;
-		const y = Math.random() * 100;
-		shape.style.transform = `translate(${x}vw, ${y}vh)`;
-	}, 5000);
-});
+import './style.css';
+// import particlesJS from 'particles.js';
+particlesJS.load('particles-js', 'assets/particles.json');
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'border-to': '#EE2A7B',
+				'border-from': '#6228D7',
+				yellow: '#F9CE34',
+			},
+		},
 	},
 	plugins: [],
 };
