5c6272f64b7f0f23ffe6002bf1ed4644730868ee
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2024-08-17 00:16:11 +0530

    Fixing styling  bugs

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 import './style.css';
 // import particlesJS from 'particles.js';
 particlesJS.load('particles-js', 'assets/particles.json');
-
-document.querySelector('.projects').addEventListener('click', function (e) {
-	const btn = e.target.closest('#view');
-	if (!btn) return;
-	btn.querySelector('i').classList.toggle('rotate-180');
-	const paragraph = btn.parentElement.parentElement.querySelector('p');
-	paragraph.classList.toggle('hidden');
-});
